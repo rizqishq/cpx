4da220a1a7c72629809f9643632f777035aaf3da
haq <210781681+rizqishq@users.noreply.github.com>
2026-06-05 21:26:11 +0700

    set release version via goreleaser ldflags

--- version.go
+++ version.go
@@ -3,11 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
+	"regexp"
+	"runtime/debug"
 )
 
 var version = "dev"
 
+var pseudoVersionPattern = regexp.MustCompile(`^v\d+\.\d+\.\d+-\d{14}-[0-9a-f]+(?:\+dirty)?$`)
+
+func isReleaseLikeVersion(value string) bool {
+	if value == "" || value == "(devel)" {
+		return false
+	}
+	if pseudoVersionPattern.MatchString(value) {
+		return false
+	}
+	return true
+}
+
+func resolvedVersion() string {
+	if version != "" && version != "dev" {
+		return version
+	}
+
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok && buildInfo != nil {
+		if isReleaseLikeVersion(buildInfo.Main.Version) {
+			return buildInfo.Main.Version
+		}
+	}
+
+	return "dev"
+}
+
 func cmdVersion(stdout io.Writer) error {
-	_, err := fmt.Fprintf(stdout, "cpx %s\n", version)
+	_, err := fmt.Fprintf(stdout, "cpx %s\n", resolvedVersion())
 	return err
 }
